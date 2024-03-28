92cf625d34785d5bc62dd7c73474f5313ac1aeb7
Bainur13 <urbain.dumas@gmail.com>
2024-03-28 02:04:18 +0100

    fix pipe chain

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vda-conc <vda-conc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: udumas <udumas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 16:34:27 by vda-conc          #+#    #+#             */
-/*   Updated: 2024/03/28 00:34:15 by vda-conc         ###   ########.fr       */
+/*   Updated: 2024/03/28 02:03:23 by udumas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ t_token	*ft_lexer(char *input, t_list **env)
 	if (!listed_tokens)
 		return (NULL);
 	ft_reunite_tokens(&listed_tokens);
-	ft_print_token_list(&listed_tokens);
 	status = ft_redirections(&listed_tokens, env);
 	if (status == -1 || status == -1917)
 		return (ft_tokenlstclear(&listed_tokens), NULL);
